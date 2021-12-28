bf3e8f6d5fee1786b3444705d94e54385abf2b76
Beep8172 <96782981+Beep8172@users.noreply.github.com>
2021-12-28 15:57:23 +0100

    Add files via upload

---
+++ client/main.lua
@@ -0,0 +1,145 @@
+Citizen.CreateThread(function()
+	-- Variables internas
+	ESX               = nil
+	local Timeouts    = {}
+	local GUI         = {}
+	GUI.Time          = 0
+	local MenuType    = 'dialog'
+	local OpenedMenus = {}
+	local soundOn = true
+
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Citizen.Wait(0)
+	end
+
+	local openMenu = function(namespace, name, data)
+		for i=1, #Timeouts, 1 do
+			ESX.ClearTimeout(Timeouts[i])
+		end
+
+		OpenedMenus[namespace .. '_' .. name] = true
+
+		SendNUIMessage({
+			action    = 'openMenu',
+			namespace = namespace,
+			name      = name,
+			data      = data
+		})
+
+		local timeoutId = ESX.SetTimeout(200, function()
+			SetNuiFocus(true, true)
+		end)
+
+		table.insert(Timeouts, timeoutId)
+	end
+
+	local closeMenu = function(namespace, name)
+		OpenedMenus[namespace .. '_' .. name] = nil
+		local OpenedMenuCount                 = 0
+
+		SendNUIMessage({
+			action    = 'closeMenu',
+			namespace = namespace,
+			name      = name,
+			data      = data
+		})
+
+		for k,v in pairs(OpenedMenus) do
+			if v == true then
+				OpenedMenuCount = OpenedMenuCount + 1
+			end
+		end
+
+		if OpenedMenuCount == 0 then
+			SetNuiFocus(false)
+		end
+
+	end
+
+	ESX.UI.Menu.RegisterType(MenuType, openMenu, closeMenu)
+
+	RegisterNUICallback('menu_submit', function(data, cb)
+		local menu = ESX.UI.Menu.GetOpened(MenuType, data._namespace, data._name)
+		local post = true
+
+		if menu.submit ~= nil then
+
+			-- Los datos enviados son un número?
+			if tonumber(data.value) ~= nil then
+
+				-- Valores flotantes redondos
+				data.value = ESX.Math.Round(tonumber(data.value))
+
+				-- Verificar valor negativo
+				if tonumber(data.value) <= 0 then
+					post = false
+				end
+			end
+
+			data.value = ESX.Math.Trim(data.value)
+
+			-- No publiques si el valor es negativo o si es 0
+			if post then
+				menu.submit(data, menu)
+				if soundOn == true then
+					PlaySound(0, "Menu_Accept", "Phone_SoundSet_Default", 0, 0, 1);
+				end
+			else
+				ESX.ShowNotification('Ese numero no es valido!')
+			end
+		end
+
+		cb('OK')
+	end)
+
+	RegisterNUICallback('menu_cancel', function(data, cb)
+		local menu = ESX.UI.Menu.GetOpened(MenuType, data._namespace, data._name)
+
+		if menu.cancel ~= nil then
+			menu.cancel(data, menu)
+			if soundOn == true then
+				PlaySound(0, "Click_Fail", "WEB_NAVIGATION_SOUNDS_PHONE", 0, 0, 1);			
+			end
+		end
+
+		cb('OK')
+	end)
+
+	RegisterNUICallback('menu_change', function(data, cb)
+		local menu = ESX.UI.Menu.GetOpened(MenuType, data._namespace, data._name)
+
+		if menu.change ~= nil then
+			menu.change(data, menu)
+		end
+
+		cb('OK')
+	end)
+
+	Citizen.CreateThread(function()
+		while true do
+			Citizen.Wait(10)
+			local OpenedMenuCount = 0
+
+			for k,v in pairs(OpenedMenus) do
+				if v == true then
+					OpenedMenuCount = OpenedMenuCount + 1
+				end
+			end
+
+			if OpenedMenuCount > 0 then
+				DisableControlAction(0, 1,   true) -- LookLeftRight
+				DisableControlAction(0, 2,   true) -- LookUpDown
+				DisableControlAction(0, 142, true) -- MeleeAttackAlternate
+				DisableControlAction(0, 106, true) -- VehicleMouseControlOverride
+				DisableControlAction(0, 12, true) -- WeaponWheelUpDown
+				DisableControlAction(0, 14, true) -- WeaponWheelNext
+				DisableControlAction(0, 15, true) -- WeaponWheelPrev
+				DisableControlAction(0, 16, true) -- SelectNextWeapon
+				DisableControlAction(0, 17, true) -- SelectPrevWeapon
+			else
+				Citizen.Wait(500)
+			end
+		end
+	end)
+end)
